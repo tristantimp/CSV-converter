5f21e88d228528fe5510790cf93f1274081f3eac
Tristan <tristantimp@gmail.com>
2024-03-06 11:29:21 +0100

    new api and html

--- flask_app.py
+++ flask_app.py
@@ -1,32 +1,36 @@
-from flask import Flask, request, render_template
-import os
+from flask import Flask, render_template, request, redirect, url_for, send_file
 import csv
+import os
 
-app = Flask(__name__, template_folder="baller")
+app = Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
 @app.route('/convert', methods=['POST'])
 def convert():
-    csv_file = request.files['csvFile']
-    if csv_file:
-        csv_filename = csv_file.filename
-        csv_file.save(csv_filename)
-        
-        lst_filename = os.path.splitext(csv_filename)[0] + '.lst'
-        csv_directory = os.path.dirname(os.path.abspath(csv_filename))  # Extract directory path
-        
-        with open(csv_filename, 'r') as csv_file:
-            reader = csv.reader(csv_file)
-            with open(os.path.join(csv_directory, lst_filename), 'w') as lst_file:  # Save .lst file in the same directory
-                for row in reader:
-                    lst_file.write(','.join(row) + '\n')
-                
-        return 'Conversion Completed! The .lst file should appear in the same location as your .csv file.'
-    else:
-        return 'Error: No file provided.'
+    if 'file' not in request.files:
+        return redirect(request.url)
+
+    file = request.files['file']
+    if file.filename == '':
+        return redirect(request.url)
+
+    if file:
+        filename = file.filename
+        if filename.endswith('.csv'):
+            csv_data = file.read().decode('utf-8')
+            csv_list = list(csv.reader(csv_data.splitlines()))
+            lst_filename = filename[:-4] + '.lst'
+
+            with open(lst_filename, 'w') as lst_file:
+                for row in csv_list:
+                    lst_file.write('\t'.join(row) + '\n')
+
+            return send_file(lst_filename, as_attachment=True)
+        else:
+            return render_template('error.html', message='Uploaded file is not a CSV file.')
 
 if __name__ == '__main__':
-    app.run(port=8000,debug=False)
+    app.run(port=8000,debug=True)
